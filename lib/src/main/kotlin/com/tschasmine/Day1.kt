65bd88c36b661e4c6ac6fa0eec39daa58326b812
Jasmin Börsig <jasmin.boersig@persolog.com>
2025-01-02 13:43:47 +0100

    Day 1: make test parse

--- lib/src/main/kotlin/com/tschasmine/Day1.kt
+++ lib/src/main/kotlin/com/tschasmine/Day1.kt
@@ -3,6 +3,23 @@ package com.tschasmine
 class Day1 {
 
     fun calculateTotalDistance(input: String): Int {
-        return 0
+        val numberSets = parseInput(input)
+        return numberSets.pair().sumOf { numberSet ->
+            kotlin.math.abs(numberSet.first - numberSet.second)
+        }
     }
+
+    private fun parseInput(input: String): NumberSets {
+        val pairs = input.lines().map { line ->
+            val (firstNo, _, secondNo) = Regex("(\\d+)(\\s+)(\\d+)").find(line.trim())?.destructured
+                ?: throw IllegalArgumentException("Invalid input")
+            firstNo.toInt() to secondNo.toInt()
+        }
+
+        return NumberSets(pairs.map { it.first }.sorted(), pairs.map { it.second }.sorted())
+    }
+}
+
+data class NumberSets(val firstNumberSet: List<Int>, val secondNumberSet: List<Int>) {
+    fun pair() = firstNumberSet.zip(secondNumberSet)
 }
